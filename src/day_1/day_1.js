9156b0865f855e48896646338b03f8f7fb69aa37
valglez <vsalglez@gmail.com>
2022-10-25 16:04:17 +0200

    completed part two

--- src/day_1/day_1.js
+++ src/day_1/day_1.js
@@ -1,4 +1,22 @@
 class Measurement {
+  group(data) {
+    try {
+      let arrs = [],
+        size = 3;
+      for (let i = 0; i < data.length; i++) arrs.push(data.slice(i, i + size));
+      return arrs.splice(0, data.length - 2);
+    } catch (error) {
+      console.error("Error while grouping: ", error.message || error);
+    }
+  }
+  sum(data) {
+    try {
+      const arr = this.group(data)
+      return arr.map(i => i.reduce((sum, item) => sum += Number(item), 0));
+    } catch (error) {
+      console.error("Error while calculating; ", error.message || error);
+    }
+  }
   calculate(data) {
     let counter = 0;
     try {
--- test/day_1/day_1.test.js
+++ test/day_1/day_1.test.js
@@ -10,7 +10,9 @@ const measurement = new Measurement();
 
 describe("Day 1", () => {
   const mockRows = loader.load(mockPath);
+  const mock2Rows = loader.load(mock2Path);
   const dataRows = loader.load(dataPath);
+  const data2Rows = loader.load(data2Path);
   describe("Depth measurement", () => {
     it("Should be the same as the resolved one.", () => {
       const value = measurement.calculate(mockRows);
@@ -19,6 +21,33 @@ describe("Day 1", () => {
     it("Should be the same as the resolved one.", () => {
       const value = measurement.calculate(dataRows);
       expect(value).toEqual(1559);
+    });
+    it("Should return triple sliding arrays.", () => {
+      const value = measurement.group(mock2Rows);
+      expect(value).toEqual([
+        ["199", "200", "208"],
+        ["200", "208", "210"],
+        ["208", "210", "200"],
+        ["210", "200", "207"],
+        ["200", "207", "240"],
+        ["207", "240", "269"],
+        ["240", "269", "260"],
+        ["269", "260", "263"],
+      ]);
+    });
+    it("Should return an array with the sum of each triple.", () => {
+      const value = measurement.sum(mock2Rows);
+      expect(value).toEqual([607, 618, 618, 617, 647, 716, 769, 792]);
+    });
+    it("Should be the same as the resolved one.", () => {
+      const sum = measurement.sum(mock2Rows)
+      const value = measurement.calculate(sum);
+      expect(value).toEqual(5);
+    });
+    it("Should be the same as the resolved one.", () => {
+      const sum = measurement.sum(data2Rows)
+      const value = measurement.calculate(sum);
+      expect(value).toEqual(1600);
     });
   });
 });
