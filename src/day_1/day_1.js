ec6754e5cc00cc0a57c04910ca6356ce4d0308ee
Valglez <71898653+valglez@users.noreply.github.com>
2022-10-25 16:23:40 +0200

    feature(day_1): part two #1

--- src/day_1/day_1.js
+++ src/day_1/day_1.js
@@ -1,4 +1,21 @@
 class Measurement {
+  group(data, size) {
+    try {
+      let arrs = [];
+      for (let i = 0; i < data.length; i++) arrs.push(data.slice(i, i + size));
+      return arrs.splice(0, data.length - 2);
+    } catch (error) {
+      console.error("Error while grouping: ", error.message || error);
+    }
+  }
+  sum(data) {
+    try {
+      const arr = this.group(data, 3);
+      return arr.map((i) => i.reduce((sum, item) => (sum += Number(item)), 0));
+    } catch (error) {
+      console.error("Error while calculating; ", error.message || error);
+    }
+  }
   calculate(data) {
     let counter = 0;
     try {
--- test/day_1/loader.test.js
+++ test/day_1/loader.test.js
@@ -1,6 +1,8 @@
 const Loader = require("../../src/utils/loader");
 const mockPath = "./test/day_1/mock.txt";
+const mock2Path = "./test/day_1/mock_2.txt";
 const dataPath = "./test/day_1/data.txt";
+const data2Path = "./test/day_1/data_2.txt";
 
 const loader = new Loader();
 
@@ -12,6 +14,14 @@ describe("Loader", () => {
     });
     it("Number of my puzzle rows should be correct.", () => {
       const rows = loader.load(dataPath);
+      expect(rows.length).toEqual(2000);
+    });
+    it("Number of rows should be correct.", () => {
+      const rows = loader.load(mock2Path);
+      expect(rows.length).toEqual(10);
+    });
+    it("Number of my puzzle rows should be correct.", () => {
+      const rows = loader.load(data2Path);
       expect(rows.length).toEqual(2000);
     });
   });
