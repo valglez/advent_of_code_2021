422897517f5f5b7304ab90105519062cc40469b2
valglez <vsalglez@gmail.com>
2022-10-26 16:22:37 +0200

    added test for decode method

--- src/day_2/decoder.js
+++ src/day_2/decoder.js
@@ -3,10 +3,25 @@ class Decoder {
     try {
       const size = data.length;
       let arrs = [];
-      for (let i = 0; i < size; i+=2) arrs.push(data.slice(i , i + 2));
+      for (let i = 0; i < size; i += 2) arrs.push(data.slice(i, i + 2));
       return arrs;
     } catch (error) {
       console.error("Error while grouping: ", error.message || error);
+    }
+  }
+  decode(data) {
+    const instruction = this.formatData(data);
+    try {
+      switch (instruction[0][0]) {
+        case "up":
+          return { direction: 1, inc: Number(instruction[0][1]) };
+        case "down":
+          return { direction: 2, inc: Number(instruction[0][1]) };
+        case "forward":
+          return { direction: 0, inc: Number(instruction[0][1]) };
+      }
+    } catch (error) {
+      console.error(error.message || error);
     }
   }
 }
--- test/day_2/day_2.test.js
+++ test/day_2/day_2.test.js
@@ -19,5 +19,17 @@ describe("Day 2", () => {
         ["forward", "2"],
       ]);
     });
+    it("Should be the same as the resolved one.", () => {
+      const value = decoder.decode(["forward", "5"]);
+      expect(value).toEqual({ direction: 0, inc: 5 });
+    });
+    it("Should be the same as the resolved one.", () => {
+      const value = decoder.decode(["up", "3"]);
+      expect(value).toEqual({ direction: 1, inc: 3 });
+    });
+    it("Should be the same as the resolved one.", () => {
+      const value = decoder.decode(["down", "8"]);
+      expect(value).toEqual({ direction: 2, inc: 8 });
+    });
   });
 });
