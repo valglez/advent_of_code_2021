f66bf0e6374d6edda3b0114cbd5a65645cc41376
valglez <vsalglez@gmail.com>
2022-10-28 00:15:38 +0200

    WIP: test for movement method

--- src/day_2/decoder.js
+++ src/day_2/decoder.js
@@ -1,26 +1,54 @@
 class Decoder {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+  }
   formatInstruction(data) {
     try {
-      const size = data.length;
       let arrs = [];
-      for (let i = 0; i < size; i += 2) arrs.push(data.slice(i, i + 2));
+      for (let i = 0; i < data.length; i += 2) arrs.push(data.slice(i, i + 2));
+      for (let i in arrs) {
+        arrs[i][1] = +arrs[i][1];
+      }
       return arrs;
     } catch (error) {
       console.error("Error while grouping: ", error.message || error);
     }
   }
-  decode(data) {
-    const instruction = this.formatInstruction(data);
+  iterate(data) {
     try {
-      for (let i = 0; i < instruction.length; i++) {
-        switch (instruction[i][0]) {
-          case "up":
-            return { direction: 1, inc: Number(instruction[0][1]) };
-          case "down":
-            return { direction: 2, inc: Number(instruction[0][1]) };
-          case "forward":
-            return { direction: 0, inc: Number(instruction[0][1]) };
-        }
+      for (let i = 0; i < data.length; i++) {
+        this.decode(data[i]);
+      }
+    } catch (error) {
+      console.error("Error while grouping: ", error.message || error);
+    }
+  }
+  decode(data) {
+    try {
+      if (data[0] == "up") {
+        this.movement({ direction: 1, inc: data[1] });
+      }
+      if (data[0] == "down") {
+        this.movement({ direction: 2, inc: data[1] });
+      }
+      if (data[0] == "forward") {
+        this.movement({ direction: 0, inc: data[1] });
+      }
+    } catch (error) {
+      console.error(error.message || error);
+    }
+  }
+  movement(data) {
+    try {
+      if (data.direction == "0") {
+        // this.x = +data.inc;
+      }
+      if (data.direction == "1") {
+        // this.y = -data.inc;
+      }
+      if (data.direction == "2") {
+        // this.y = +data.inc;
       }
     } catch (error) {
       console.error(error.message || error);
--- test/day_2/decoder.test.js
+++ test/day_2/decoder.test.js
@@ -11,25 +11,17 @@ describe("Day 2", () => {
     it("Should be the same as the resolved one.", () => {
       const value = decoder.formatInstruction(mockRows);
       expect(value).toEqual([
-        ["forward", "5"],
-        ["down", "5"],
-        ["forward", "8"],
-        ["up", "3"],
-        ["down", "8"],
-        ["forward", "2"],
+        ["forward", 5],
+        ["down", 5],
+        ["forward", 8],
+        ["up", 3],
+        ["down", 8],
+        ["forward", 2],
       ]);
     });
-    it("Should be the instruction for 'forward'.", () => {
-      const value = decoder.decode(["forward", "5"]);
-      expect(value).toEqual({ direction: 0, inc: 5 });
-    });
-    it("Should be the instruction for 'up'.", () => {
-      const value = decoder.decode(["up", "3"]);
-      expect(value).toEqual({ direction: 1, inc: 3 });
-    });
-    it("Should be the instruction for 'down'.", () => {
-      const value = decoder.decode(["down", "8"]);
-      expect(value).toEqual({ direction: 2, inc: 8 });
+    it("Should be the same as the resolved one.", () => {
+      const value = decoder.movement(mockRows);
+      expect(value).toEqual(0);
     });
   });
 });
