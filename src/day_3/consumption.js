c6631981e566cec1c309ed40e8dcfb84370eab7a
Valglez <71898653+valglez@users.noreply.github.com>
2022-11-08 14:12:50 +0100

    feature(day_3): part two (#5)

--- src/day_3/consumption.js
+++ src/day_3/consumption.js
@@ -2,14 +2,6 @@ class Consumption {
   constructor() {
     this.gamma = [];
     this.epsilon = [];
-    this.decimalGamma = 0;
-    this.decimalEpsilon = 0;
-  }
-  exec(arr) {
-    for (let i = 0; i < arr[0].length; i++) {
-      this.getMostCommonBit(this.getColumn(arr, i));
-      this.getLeastCommonBit(this.getColumn(arr, i));
-    }
   }
   getColumn(arr, n) {
     let column = [];
@@ -21,47 +13,99 @@ class Consumption {
   getMostCommonBit(col) {
     let countOne = 0;
     for (let i = 0; i < col.length; i++) {
-      if (col[i] == 1) {
-        countOne++;
-      }
+      col[i] == 1 ? countOne++ : 0;
     }
-    if (countOne > col.length / 2) {
-      return (this.gamma += "1");
-    }
-    return (this.gamma += "0");
+    return countOne > col.length / 2 ? (this.gamma += 1) : (this.gamma += 0);
   }
   getLeastCommonBit(col) {
     let countOne = 0;
     for (let i = 0; i < col.length; i++) {
-      if (col[i] == 1) {
-        countOne++;
-      }
+      col[i] == 1 ? countOne++ : 0;
     }
-    if (countOne > col.length / 2) {
-      return (this.epsilon += "0");
+    return countOne > col.length / 2
+      ? (this.epsilon += 0)
+      : (this.epsilon += 1);
+  }
+  exec(arr) {
+    for (let i = 0; i < arr[0].length; i++) {
+      this.getMostCommonBit(this.getColumn(arr, i));
+      this.getLeastCommonBit(this.getColumn(arr, i));
     }
-    return (this.epsilon += "1");
+  }
+  getMostCommonBitForCriteria(col) {
+    let countOne = 0;
+    let countZero = 0;
+    for (let i = 0; i < col.length; i++) {
+      col[i] == 0 ? countZero++ : countOne++;
+    }
+    return countZero > countOne ? "0" : "1";
+  }
+  getLeastCommonBitForCriteria(col) {
+    let countOne = 0;
+    let countZero = 0;
+    for (let i = 0; i < col.length; i++) {
+      col[i] == 0 ? countZero++ : countOne++;
+    }
+    return countZero > countOne ? "1" : "0";
+  }
+  filterElementByCharInPosition(arr, char, position) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      arr[i].charAt(position) == char ? result.push(arr[i]) : 0;
+    }
+    return result;
+  }
+  getOxygenBinary(arr) {
+    let posicion = 0;
+    while (arr.length > 1) {
+      arr = this.filterElementByCharInPosition(
+        arr,
+        this.getMostCommonBitForCriteria(this.getColumn(arr, posicion)),
+        posicion
+      );
+      posicion++;
+    }
+    return arr;
+  }
+  getCO2Binary(arr) {
+    let posicion = 0;
+    while (arr.length > 1) {
+      arr = this.filterElementByCharInPosition(
+        arr,
+        this.getLeastCommonBitForCriteria(this.getColumn(arr, posicion)),
+        posicion
+      );
+      posicion++;
+    }
+    return arr;
   }
   getGamma() {
     return this.gamma;
   }
-  getDecimalGamma(bitGamma) {
-    return (this.decimalGamma += parseInt(bitGamma, 2));
+  getDecimalGamma() {
+    return parseInt(this.gamma, 2);
   }
   getEpsilon() {
     return this.epsilon;
   }
-  getDecimalEpsilon(bitEpsilon) {
-    return (this.decimalEpsilon += parseInt(bitEpsilon, 2));
+  getDecimalEpsilon() {
+    return parseInt(this.epsilon, 2);
   }
-  getMultiply() {
-    return this.decimalGamma * this.decimalEpsilon;
+  getGammaEpsilonMultiply() {
+    return this.getDecimalGamma() * this.getDecimalEpsilon();
+  }
+  getDecimalOxygen(res) {
+    return parseInt(this.getOxygenBinary(res), 2);
+  }
+  getDecimalCO2(res) {
+    return parseInt(this.getCO2Binary(res), 2);
+  }
+  getOxygenCO2Multiply(res) {
+    return this.getDecimalOxygen(res) * this.getDecimalCO2(res);
   }
   reset() {
     this.gamma = [];
     this.epsilon = [];
-    this.decimalEpsilon = 0;
-    this.decimalGamma = 0;
   }
 }
 
--- test/day_3/consumption.test.js
+++ test/day_3/consumption.test.js
@@ -51,53 +51,112 @@ describe("Day 3", () => {
       const value = consumption.getLeastCommonBit(colArray);
       expect(value).toEqual("1");
     });
-    it("Should be 10110.", () => {
+    it("Should be the gamma rate in binary.", () => {
       consumption.reset();
       consumption.exec(mockRows);
       const value = consumption.getGamma();
       expect(value).toEqual("10110");
     });
-    it("Should be 22.", () => {
-      const gamma = consumption.getGamma();
-      const value = consumption.getDecimalGamma(gamma);
+    it("Should be the gamma rate in decimal.", () => {
+      const value = consumption.getDecimalGamma();
       expect(value).toEqual(22);
     });
-    it("Should be 01001.", () => {
+    it("Should be the epsilon rate in binary.", () => {
       const value = consumption.getEpsilon();
       expect(value).toEqual("01001");
     });
-    it("Should be 9.", () => {
-      const epsilon = consumption.getEpsilon();
-      const value = consumption.getDecimalEpsilon(epsilon);
+    it("Should be the epsilon rate in decimal.", () => {
+      const value = consumption.getDecimalEpsilon();
       expect(value).toEqual(9);
     });
-    it("Should be 198.", () => {
-      value = consumption.getMultiply();
+    it("Should be the multiplication of the gamma rate and the epsilon rate.", () => {
+      value = consumption.getGammaEpsilonMultiply();
       expect(value).toEqual(198);
     });
-    it("Should be 000010111110.", () => {
+    it("Should be the gamma rate in binary.", () => {
       consumption.reset();
       consumption.exec(dataRows);
       const value = consumption.getGamma();
       expect(value).toEqual("000010111110");
     });
-    it("Should be 190.", () => {
-      const gamma = consumption.getGamma();
-      const value = consumption.getDecimalGamma(gamma);
+    it("Should be the gamma rate in decimal.", () => {
+      const value = consumption.getDecimalGamma();
       expect(value).toEqual(190);
     });
-    it("Should be 111101000001.", () => {
+    it("Should be the epsilon rate in binary.", () => {
       const value = consumption.getEpsilon();
       expect(value).toEqual("111101000001");
     });
-    it("Should be 3905.", () => {
-      const epsilon = consumption.getEpsilon();
-      const value = consumption.getDecimalEpsilon(epsilon);
+    it("Should be the decimal rate in binary.", () => {
+      const value = consumption.getDecimalEpsilon();
       expect(value).toEqual(3905);
     });
-    it("Should be 741950.", () => {
-      value = consumption.getMultiply();
+    it("Should be the multiplication of the gamma rate and the epsilon rate.", () => {
+      value = consumption.getGammaEpsilonMultiply();
       expect(value).toEqual(741950);
+    });
+    it("Should be an array with the filter element by this char and position.", () => {
+      const recd = [
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "11100",
+        "10000",
+        "11001",
+      ];
+      const value = consumption.filterElementByCharInPosition(mockRows, 1, 0);
+      expect(value).toEqual(recd);
+    });
+    it("Should be the most common bit for this column.", () => {
+      const col = consumption.getColumn(mockRows, 0);
+      const value = consumption.getMostCommonBitForCriteria(col);
+      expect(value).toEqual("1");
+    });
+    it("Should be the oxygen generator rating in binary.", () => {
+      const value = consumption.getOxygenBinary(mockRows);
+      expect(value).toEqual(["10111"]);
+    });
+    it("Should be the oxygen generator rating in binary.", () => {
+      const value = consumption.getOxygenBinary(dataRows);
+      expect(value).toEqual(["000100011010"]);
+    });
+    it("Should be the oxygen generator rating in decimal.", () => {
+      const value = consumption.getDecimalOxygen(mockRows);
+      expect(value).toEqual(23);
+    });
+    it("Should be the oxygen generator rating in decimal.", () => {
+      const value = consumption.getDecimalOxygen(dataRows);
+      expect(value).toEqual(282);
+    });
+    it("Should be the most common bit for this column.", () => {
+      const col = consumption.getColumn(mockRows, 0);
+      const value = consumption.getLeastCommonBitForCriteria(col);
+      expect(value).toEqual("0");
+    });
+    it("Should be the oxygen generator rating in binary.", () => {
+      const value = consumption.getCO2Binary(mockRows);
+      expect(value).toEqual(["01010"]);
+    });
+    it("Should be the oxygen generator rating in binary.", () => {
+      const value = consumption.getCO2Binary(dataRows);
+      expect(value).toEqual(["110010000101"]);
+    });
+    it("Should be CO2 scrubber rating in decimal.", () => {
+      const value = consumption.getDecimalCO2(mockRows);
+      expect(value).toEqual(10);
+    });
+    it("Should be CO2 scrubber rating in decimal.", () => {
+      const value = consumption.getDecimalCO2(dataRows);
+      expect(value).toEqual(3205);
+    });
+    it("Should be the multiplication of the oxygen generator rate and the CO2 scrubber rate.", () => {
+      const value = consumption.getOxygenCO2Multiply(mockRows);
+      expect(value).toEqual(230);
+    });
+    it("Should be the multiplication of the oxygen generator rate and the CO2 scrubber rate.", () => {
+      const value = consumption.getOxygenCO2Multiply(dataRows);
+      expect(value).toEqual(903810);
     });
   });
 });
